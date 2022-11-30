d5047a42372935a83b42bf4ea4015108cd64ea3a
Duyen-codes <hongduyen0705@gmail.com>
2022-11-30 10:03:21 +0200

    extract books and authors array to their own files

--- library-backend/index.js
+++ library-backend/index.js
@@ -29,84 +29,6 @@ mongoose
 		console.log("error connection to MongoDB: ", error.message);
 	});
 
-let authors = [
-	{
-		name: "Robert Martin",
-		id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-		born: 1952,
-	},
-	{
-		name: "Martin Fowler",
-		id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-		born: 1963,
-	},
-	{
-		name: "Fyodor Dostoevsky",
-		id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-		born: 1821,
-	},
-	{
-		name: "Joshua Kerievsky", // birthyear not known
-		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-	},
-	{
-		name: "Sandi Metz", // birthyear not known
-		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-	},
-];
-
-let books = [
-	{
-		title: "Clean Code",
-		published: 2008,
-		author: "Robert Martin",
-		id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"],
-	},
-	{
-		title: "Agile software development",
-		published: 2002,
-		author: "Robert Martin",
-		id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-		genres: ["agile", "patterns", "design"],
-	},
-	{
-		title: "Refactoring, edition 2",
-		published: 2018,
-		author: "Martin Fowler",
-		id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"],
-	},
-	{
-		title: "Refactoring to patterns",
-		published: 2008,
-		author: "Joshua Kerievsky",
-		id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "patterns"],
-	},
-	{
-		title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-		published: 2012,
-		author: "Sandi Metz",
-		id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "design"],
-	},
-	{
-		title: "Crime and punishment",
-		published: 1866,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "crime"],
-	},
-	{
-		title: "The Demon ",
-		published: 1872,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "revolution"],
-	},
-];
-
 const typeDefs = gql`
 	type Author {
 		name: String!
@@ -117,9 +39,9 @@ const typeDefs = gql`
 
 	type Book {
 		title: String!
-		published: Int!
+		published: Int
 		author: Author!
-		genres: [String!]!
+		genres: [String!]
 		id: ID!
 	}
 
@@ -149,9 +71,9 @@ const typeDefs = gql`
 	type Mutation {
 		addBook(
 			title: String!
+			published: Int
 			author: String!
-			published: Int!
-			genres: [String!]!
+			genres: [String!]
 		): Book!
 
 		editAuthor(name: String!, setBornTo: Int!): Author
@@ -167,28 +89,6 @@ const resolvers = {
 		authorCount: async () => Author.collection.countDocuments(),
 		bookCount: async () => Book.collection.countDocuments(),
 		allAuthors: async () => {
-			// let counter = [];
-			// const bookLength = await Book.collection.countDocuments();
-			// const books = await Book.find({}).populate("author", { name: 1 });
-
-			// const authors = await Author.find({});
-
-			// for (let i = 0; i < bookLength; i++) {
-			// 	if (counter[books[i].author.name]) {
-			// 		counter[books[i].author.name] += 1;
-			// 	} else {
-			// 		counter[books[i].author.name] = 1;
-			// 	}
-			// }
-
-			// const reformattedArray = authors.map((author) => ({
-			// 	id: author._id.toString(),
-			// 	name: author.name,
-			// 	born: author.born || null,
-			// 	bookCount: counter[author.name] || 0,
-			// }));
-
-			// return reformattedArray;
 			return Author.find({});
 		},
 		allBooks: async (root, args) => {
@@ -298,7 +198,6 @@ const resolvers = {
 				username: args.username,
 				favoriteGenre: args.favoriteGenre,
 			});
-
 			return user.save().catch((error) => {
 				throw new UserInputError(error.message, {
 					invalidArgs: args,
@@ -318,7 +217,6 @@ const resolvers = {
 				username: user.username,
 				id: user._id,
 			};
-
 			return { value: jwt.sign(userForToken, JWT_SECRET) };
 		},
 	},
