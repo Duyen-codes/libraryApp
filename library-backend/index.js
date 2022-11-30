343b2345adb9f06627b31b3e60d313cad1e6936d
Duyen-codes <hongduyen0705@gmail.com>
2022-11-30 14:47:58 +0200

    refactored the backend

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,9 +1,9 @@
-const {
-	ApolloServer,
-	gql,
-	UserInputError,
-	AuthenticationError,
-} = require("apollo-server");
+const { gql, UserInputError, AuthenticationError } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const express = require("express");
+const http = require("http");
 
 const jwt = require("jsonwebtoken");
 
@@ -11,8 +11,6 @@ const JWT_SECRET = "TOP_SECRET";
 
 const mongoose = require("mongoose");
 
-const Book = require("./models/book");
-const Author = require("./models/author");
 const User = require("./models/user");
 
 const typeDefs = require("./schema");
@@ -32,22 +30,42 @@ mongoose
 		console.log("error connection to MongoDB: ", error.message);
 	});
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	context: async ({ req }) => {
-		const auth = req ? req.headers.authorization : null;
+const start = async () => {
+	const app = express();
+	const httpServer = http.createServer(app);
 
-		if (auth && auth.toLowerCase().startsWith("bearer ")) {
-			const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+	const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-			const currentUser = await User.findById(decodedToken.id);
-			console.log("currentUser", currentUser);
-			return { currentUser };
-		}
-	},
-});
+	// server
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: async ({ req }) => {
+			const auth = req ? req.headers.authorization : null;
 
-server.listen().then(({ url }) => {
-	console.log(`Server ready at ${url}`);
-});
+			if (auth && auth.toLowerCase().startsWith("bearer ")) {
+				const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+
+				const currentUser = await User.findById(decodedToken.id);
+				console.log("currentUser", currentUser);
+				return { currentUser };
+			}
+		}, // context ends
+		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	}); // server ends
+
+	await server.start();
+
+	server.applyMiddleware({
+		app,
+		path: "/",
+	});
+
+	const PORT = 4000;
+
+	httpServer.listen(PORT, () =>
+		console.log(`Server is now running on http://localhost:${PORT}`),
+	);
+}; // start func ends
+
+start();
